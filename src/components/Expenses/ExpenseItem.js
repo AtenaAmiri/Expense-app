66d049322e7f06886b88ad570c229d673a2f9fa0
atena <ftemeh.amiirii@gmail.com>
2023-06-06 21:05:53 +0330

    Expense-app

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,14 +3,16 @@ import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 function ExpenseItem(props) {
+  let title = props.title;
   const clickHandler = () => {
-    console.log("Clicked!!!");
+    title = "I'm change";
+    console.log(title);
   };
   return (
     <Card className="expense-item" className2="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
       </div>
       <div className="expense-item__price ">${props.amount}</div>
       <button onClick={clickHandler}>Change Title</button>
